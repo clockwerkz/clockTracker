ff3f159ae144549162680614ad12397881c2920d
Carlos <clockwerkz@gmail.com>
2020-09-30 18:54:35 -0700

    adding in tests for adding up time blocks

--- client/src/tests/utility.test.js
+++ client/src/tests/utility.test.js
@@ -1,4 +1,4 @@
-import { calculateMinutes, calculateTimeBlock } from '../utility/timeFunctions';
+import { calculateMinutes, calculateTimeBlock, calculateClockOutTime } from '../utility/timeFunctions';
 
 
 describe('converting timestamps into minutes with calculateMinutes:', ()=>{
@@ -23,4 +23,16 @@ describe('clock math: subtracting time stamps to calculate time spent:', ()=>{
         let result = calculateTimeBlock('00:30', '01:00');
         expect(result).toBe(30);
     });
+    test('should calculate 3:00 - 01:32 to equal 88', ()=>{
+        let result = calculateTimeBlock('01:32', '3:00');
+        expect(result).toBe(88);
+
+    })
+});
+
+describe('calculating what the time will be after adding on provided minutes', ()=>{
+    test('should calculate 8:00 to be 16:00 after adding 480 minutes',()=> {
+        let result = calculateClockOutTime('8:00', 480);
+        expect(result).toBe('16:00');
+    })
 });
