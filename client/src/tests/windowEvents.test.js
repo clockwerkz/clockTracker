e677e8a406b2061ef66431d57ce7fc04294330c1
clockwerkz <clockwerkz@gmail.com>
2020-10-17 12:11:25 -0700

    trying to do the onBlur math

--- client/src/tests/windowEvents.test.js
+++ client/src/tests/windowEvents.test.js
@@ -1,6 +1,6 @@
-import { onBlur } from '../utility/windowEvents';
+import { onBlur, onFocus } from '../utility/windowEvents';
 
-describe('Testing Focus/Blur Window Event functions:', ()=>{
+describe('Testing Blur Window Event function:', ()=>{
     test("it should return a Date object", ()=>{
         const res = onBlur();
         expect(typeof res).toBe('object');
@@ -8,5 +8,28 @@ describe('Testing Focus/Blur Window Event functions:', ()=>{
     test("it should return the current time", ()=>{
         const res = onBlur();
         expect(res).toStrictEqual(new Date());
+    })
+})
+
+describe('Testing Focus Window Event function:', ()=>{
+    test("it should accept a time one hour in the past and return 60 (min)", ()=>{
+        const startDate = new Date();
+        startDate.setHours(startDate.getHours() - 1);
+        const res = onFocus(startDate);
+        expect(startDate.getHours()).toBe(new Date().getHours() - 1);
+        expect(res).toBe(60);
+    })
+    test("it should accept a time 30 min in the past and return 30 min", ()=>{
+        const startDate = new Date();
+        if (startDate.getMinutes() < 30) {
+            let currentMinutes = startDate.getMinutes();
+            let minutes = 60 - (30 - currentMinutes);
+            startDate.setHours(startDate.getHours() - 1);
+            startDate.setMinutes(minutes);
+        } else {
+            startDate.setMinutes(startDate.getMinutes() - 30);
+        }
+        const res = onFocus(startDate);
+        expect(res).toBe(30);
     })
 })
--- client/src/utility/windowEvents.js
+++ client/src/utility/windowEvents.js
@@ -1,1 +1,13 @@
 export const onBlur = () => new Date();
+
+export const onFocus = (startDate) => {
+    const now = new Date();
+    const calcTime = (now.getHours() - startDate.getHours()) * 60;
+    if (startDate.getMinutes() >= now.getMinutes()) {
+        return calcTime + (startDate.getMinutes() - now.getMinutes());
+    } else {
+        return calcTime - now.getMinutes();
+    }
+}
+
+
