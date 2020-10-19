5afc80295c4e6ec4ae98b53f2173a839efff1390
clockwerkz <clockwerkz@gmail.com>
2020-10-18 23:09:38 -0700

    trying to handle focus/blur through react

--- client/src/App.js
+++ client/src/App.js
@@ -1,20 +1,40 @@
 import React, { useState, useEffect } from 'react';
 import TimeButton from './components/TimeButtons';
-
+import { onBlur, onFocus } from './utility/windowEvents';
 function App() {
   const [clockedIn, setClockedIn] = useState(false);
   const [ timeBank, setTimeBank ] = useState([]);
+  const [ timeBlurred, setTimeBlurred ] = useState(new Date());
   const [ count, setCount ] = useState(0);
+  const [ isBlurred, setIsBlurred ] = useState(false);
 
   useEffect(()=>{
-    console.log("Setting up timeout:");
-    setTimeout(()=> setCount(count+1), 1000);
-  }, [count])
+    window.addEventListener('blur', ()=>{
+      console.log("Blur");
+      setIsBlurred(true);
+      setTimeBlurred(new Date());
+    })
+    window.addEventListener('focus', ()=>{
+     console.log("Back in focus");
+     if (timeBlurred !== null) {
+      console.log("Time in between blur and focus:", onFocus(timeBlurred));
+     }
+     setIsBlurred(false); 
+    })
+  },[])
 
-  return (
+  useEffect(()=>{
+    if (!isBlurred) {
+      setTimeout(()=>{
+        setCount(count+1);
+      }, 1000)
+    }
+  }, [count, isBlurred])
+
+   return (
     <div className="App">
       <h1>Timebank</h1>
-      <input type="time"></input>
+      <input type="time" value={timeBlurred.getHours() + ":" + timeBlurred.getMinutes()}></input>
       <TimeButton clockedIn={clockedIn} setClockedIn={setClockedIn} />
       <p>{count}</p>
     </div>
