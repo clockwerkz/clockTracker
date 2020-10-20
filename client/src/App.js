633b6628fea2fe860c3bab493d8f181df39ac186
clockwerkz <clockwerkz@gmail.com>
2020-10-19 23:14:45 -0700

    creating timecard sheet

--- client/src/App.js
+++ client/src/App.js
@@ -3,40 +3,18 @@ import TimeButton from './components/TimeButtons';
 import { onBlur, onFocus } from './utility/windowEvents';
 function App() {
   const [clockedIn, setClockedIn] = useState(false);
-  const [ timeBank, setTimeBank ] = useState([]);
+  const [ timeBank, setTimeBank ] = useState([{in: "10:30", out: "11:30"}]);
   const [ timeBlurred, setTimeBlurred ] = useState(new Date());
-  const [ count, setCount ] = useState(0);
-  const [ isBlurred, setIsBlurred ] = useState(false);
-
-  useEffect(()=>{
-    window.addEventListener('blur', ()=>{
-      console.log("Blur");
-      setIsBlurred(true);
-      setTimeBlurred(new Date());
-    })
-    window.addEventListener('focus', ()=>{
-     console.log("Back in focus");
-     if (timeBlurred !== null) {
-      console.log("Time in between blur and focus:", onFocus(timeBlurred));
-     }
-     setIsBlurred(false); 
-    })
-  },[])
-
-  useEffect(()=>{
-    if (!isBlurred) {
-      setTimeout(()=>{
-        setCount(count+1);
-      }, 1000)
-    }
-  }, [count, isBlurred])
 
    return (
     <div className="App">
       <h1>Timebank</h1>
-      <input type="time" value={timeBlurred.getHours() + ":" + timeBlurred.getMinutes()}></input>
+      {timeBank.length > 0 ? (timeBank.map((entry, index)=><p key={index}>
+        <input type="time" value={entry.in} />
+        <input type="time" value={entry.out} />
+      </p>)):
+      ("")}
       <TimeButton clockedIn={clockedIn} setClockedIn={setClockedIn} />
-      <p>{count}</p>
     </div>
   );
 }
