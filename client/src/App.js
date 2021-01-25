f051b45bc96dbfc04a5b3f05a612923b33123a3d
Carlos <clockwerkz@gmail.com>
2021-01-24 19:02:19 -0800

    setting up focus and blur event handling

--- client/src/App.js
+++ client/src/App.js
@@ -6,6 +6,17 @@ function App() {
   const [ timeBank, setTimeBank ] = useState([{in: "10:30", out: "11:30"}]);
   const [ timeBlurred, setTimeBlurred ] = useState(new Date());
 
+  useEffect(() => {
+    
+    window.addEventListener('focus', onFocus);
+    window.addEventListener('blur', onBlur);
+    // Specify how to clean up after this effect:
+    return () => {
+      window.removeEventListener('focus', onFocus);
+      window.removeEventListener('blur', onBlur);
+    };
+  });
+
    return (
     <div className="App">
       <h1>Timebank</h1>
--- client/src/utility/windowEvents.js
+++ client/src/utility/windowEvents.js
@@ -1,13 +1,17 @@
-export const onBlur = () => new Date();
+export const onBlur = () => {
+    console.log("blurring:", new Date())
+    //return new Date();
+}
 
 export const onFocus = (startDate) => {
     const now = new Date();
-    const calcTime = (now.getHours() - startDate.getHours()) * 60;
-    if (now.getMinutes() < startDate.getMinutes()) {
-        return calcTime - (startDate.getMinutes() - now.getMinutes());
-    } else {
-        return calcTime + (now.getMinutes() - startDate.getMinutes());
-    }
+    console.log("Focus", now);
+    // const calcTime = (now.getHours() - startDate.getHours()) * 60;
+    // if (now.getMinutes() < startDate.getMinutes()) {
+    //     return calcTime - (startDate.getMinutes() - now.getMinutes());
+    // } else {
+    //     return calcTime + (now.getMinutes() - startDate.getMinutes());
+    // }
 }
 
 
