e3d4f037585f9b095e0454181144691c0760ca59
Carlos <clockwerkz@gmail.com>
2021-01-24 19:49:24 -0800

    adds an interval timer

--- client/src/App.js
+++ client/src/App.js
@@ -5,17 +5,27 @@ function App() {
   const [clockedIn, setClockedIn] = useState(false);
   const [ timeBank, setTimeBank ] = useState([{in: "10:30", out: "11:30"}]);
   const [ timeBlurred, setTimeBlurred ] = useState(new Date());
-
+  const [timeInteval, setTimeInterval] = useState(0);
+  const [count, setCount] = useState(0);
   useEffect(() => {
-    
-    window.addEventListener('focus', onFocus);
-    window.addEventListener('blur', onBlur);
+    setTimeInterval(setInterval(()=>{
+      setCount(prevTime => prevTime + 1);
+    }, 1000));
+    //window.addEventListener('focus', onFocus);
+    //window.addEventListener('blur', onBlur);
     // Specify how to clean up after this effect:
     return () => {
-      window.removeEventListener('focus', onFocus);
-      window.removeEventListener('blur', onBlur);
+      //window.removeEventListener('focus', onFocus);
+      //window.removeEventListener('blur', onBlur);
+      console.log("Clearing time Interval:");
+      clearInterval(timeInteval);
+      setTimeInterval(0);
     };
-  });
+  },[]);
+
+  useEffect(()=>{
+    console.log(count);
+  },[count])
 
    return (
     <div className="App">
